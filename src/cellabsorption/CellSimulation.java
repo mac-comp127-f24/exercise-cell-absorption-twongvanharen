8c71543925269e6908782ba8c0e5db251a4d2c75
twongvanharen <twongvan@macalester.edu>
2024-10-04 16:52:39 -0500

    Seems done, but it doesn't work

--- src/cellabsorption/CellSimulation.java
+++ src/cellabsorption/CellSimulation.java
@@ -6,70 +6,73 @@ import edu.macalester.graphics.Point;
 
 import java.awt.Color;
 import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
 
-@SuppressWarnings("SameParameterValue")
-public class CellSimulation {
-    private static final double
-        WIGGLINESS = 0.2,
-        WANDER_FROM_CENTER = 60000;
-
-    private CanvasWindow canvas;
-    private Random rand = new Random();
+public class Cell{
     private Ellipse shape;
     private double radius;
     private double direction;
+    private Random rand = new Random();
+    private static final double WIGGLINESS = 0.2;
+    private static final double WANDER_FROM_CENTER = 60000;
 
-    public static void main(String[] args) {
-        new CellSimulation();
-    }
-
-    public CellSimulation() {
-        canvas = new CanvasWindow("Cell Absorption", 800, 800);
-        populateCells();
-
-        //noinspection InfiniteLoopStatement
-        while (true) {
-            Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
-            moveAround(canvasCenter);
-            grow(0.02);
-
-            canvas.draw();
-            canvas.pause(10);
-        }
-    }
-
-    private void populateCells() {
-        double size = rand.nextInt(5) + 2;
-        createCell(
-            rand.nextDouble() * (canvas.getWidth() - size),
-            rand.nextDouble() * (canvas.getWidth() - size),
-            size,
-            Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1));
-        canvas.add(shape);
-    }
-
-    private void createCell(double x, double y, double radius, Color color) {
+    public Cell(double x, double y, double radius, Color color) {
         shape = new Ellipse(x, y, radius * 2, radius * 2);
         shape.setFillColor(color);
         this.radius = radius;
         direction = normalizeRadians(Math.random() * Math.PI * 2);
     }
-
     private void grow(double amount) {
         setRadius(radius + amount);
     }
-
-    private void setRadius(double newRadius) {
-        if (newRadius < 0) {
-            newRadius = 0;
-        }
-        radius = newRadius;
-        Point previousCenter = shape.getCenter();
-        shape.setSize(new Point(newRadius * 2, newRadius * 2));
-        shape.setCenter(previousCenter);
+        
+    public Ellipse getShape() {
+        return shape;
+    }
+    public Point getCenter() {
+        return shape.getCenter();
     }
 
-    private void moveAround(Point centerOfGravity) {
+    /**
+     * Causes this cell to interact with the other given cell. If the two
+     * cells overlap and both have a positive radius, then the larger cell
+     * absorbs area from the smaller cell so that the total area is the
+     * same, but the two cells are now tangent.
+     */
+    public void interactWith(Cell otherCell) {
+        if (radius == 0 || otherCell.radius == 0) {
+            return;
+        }
+        if (overlapAmount(otherCell) < 0) {
+            return;
+        }
+
+        if (radius > otherCell.radius) {
+            absorb(otherCell);
+        } else {
+            otherCell.absorb(this);
+        }
+    }
+
+    private double overlapAmount(Cell otherCell) {
+        return radius + otherCell.radius - getCenter().distance(otherCell.getCenter());
+    }
+
+    private void absorb(Cell otherCell) {
+        double d = getCenter().distance(otherCell.getCenter());
+        double a = sqr(radius) + sqr(otherCell.radius);
+        double newRadius = (d + Math.sqrt(2 * a - sqr(d))) / 2;
+
+        setRadius(newRadius);
+        otherCell.setRadius(d - newRadius);
+    }
+
+    private static double sqr(double x) {
+        return x * x;
+    }
+
+    public void moveAround(Point centerOfGravity) {
         shape.moveBy(Math.cos(direction), Math.sin(direction));
 
         double distToCenter = shape.getCenter().distance(centerOfGravity);
@@ -82,12 +85,71 @@ public class CellSimulation {
                 + turnTowardCenter * Math.tanh(distToCenter / WANDER_FROM_CENTER));
     }
 
-    private static double sqr(double x) {
-        return x * x;
+    private void setRadius(double newRadius) {
+        if (newRadius < 0) {
+            newRadius = 0;
+        }
+        radius = newRadius;
+        Point previousCenter = shape.getCenter();
+        shape.setSize(new Point(newRadius * 2, newRadius * 2));
+        shape.setCenter(previousCenter);
     }
 
     private static double normalizeRadians(double theta) {
         double pi2 = Math.PI * 2;
         return ((theta + Math.PI) % pi2 + pi2) % pi2 - Math.PI;
+    }
+}
+
+
+@SuppressWarnings("SameParameterValue")
+public class CellSimulation {
+    private CanvasWindow canvas;
+    private Random rand = new Random();
+    private List<Cell> cells;
+
+    public static void main(String[] args) {
+        new CellSimulation();
+    }
+
+    private void handleCellInteraction() {
+        for (int i = 0; i < cells.size(); i++) {
+            Cell cell0 = cells.get(i);
+            for (int j = i + 1; j < cells.size(); j++) {
+                Cell cell1 = cells.get(j);
+                cell0.interactWith(cell1);
+            }
+        }
+    }
+    public CellSimulation() {
+        canvas = new CanvasWindow("Cell Absorption", 800, 800);
+        populateCells();
+
+        while (true) {
+            Point canvasCenter = new Point(canvas.getWidth() / 2.0, canvas.getHeight() / 2.0);
+
+            for (Cell cell : cells) {
+                cell.moveAround(canvasCenter);
+                cell.grow(0.02);
+            }
+
+            canvas.draw();
+            canvas.pause(10);
+        }
+    }
+
+    private void populateCells() {
+        cells = new ArrayList<>();
+        for (int i = 0; i < 200; i++) {
+            double size = rand.nextInt(5) + 2;
+            Cell cell = new Cell(
+                rand.nextDouble() * (canvas.getWidth() - size),
+                rand.nextDouble() * (canvas.getHeight() - size),
+                size,
+                Color.getHSBColor(rand.nextFloat(), rand.nextFloat() * 0.5f + 0.1f, 1)
+            );
+            cells.add(cell);
+            canvas.add(cell.getShape());
+        }
     }
 }
